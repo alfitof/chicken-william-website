eac15955d3e44925a2a1d76b81de4868f7fc4e17
alfitof <alfito.fbriansyah@gmail.com>
2023-03-29 11:25:41 +0700

    Add lang element

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
           <Hero />
           {/* <Banner /> */}
           <Services />
-          <ProductSection />
+          {/* <ProductSection /> */}
         </main>
       </div>
     </Layout>
