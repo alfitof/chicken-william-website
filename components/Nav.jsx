f4b898ad4fb874d8d68a343cce9f15e981adb5f5
alfitof <alfito.fbriansyah@gmail.com>
2023-04-09 01:51:06 +0700

    Fix bug invoice 3

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,7 +15,7 @@ import Logo from "../assets/Logo.png";
 import css from "../styles/Header.module.css";
 
 const Nav = () => {
-  const { session } = useSession();
+  const { data: session } = useSession();
   const [Order, setOrder] = useState("");
   useEffect(() => {
     setOrder(localStorage.getItem("order"));
