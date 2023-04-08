03ed1cf86a54e7005fe64806bde218159c294170
alfitof <alfito.fbriansyah@gmail.com>
2023-04-09 01:32:02 +0700

    add NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({
   Component,
@@ -8,6 +9,13 @@ export default function App({
   return (
     <SessionProvider session={session}>
       {" "}
+      <NextNProgress
+        color="#29d"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />{" "}
     </SessionProvider>
   );
